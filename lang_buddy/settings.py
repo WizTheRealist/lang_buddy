fdccf3292707bc6ad2449cf0b4d46787a7569bfa
Chimaobi Ubani <wiztherealist@gmail.com>
2025-09-17 02:09:54 +0100

    ready for production

--- lang_buddy/settings.py
+++ lang_buddy/settings.py
@@ -35,11 +35,11 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'lang_buddy.onrender.com',  # Allow all Render subdomains
+    'lang-buddy.onrender.com',  # Allow all Render subdomains
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://lang_buddy.onrender.com"
+    "https://lang-buddy.onrender.com"
 ]
 
 
